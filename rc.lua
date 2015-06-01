0e9d6faeb7ad6829747e3f03d1d886b594523741
Kazuki Maeda <kmaeda@kmaeda.net>
2015-06-01 13:57:24 +0900

    Update email.

--- rc.lua
+++ rc.lua
@@ -1,5 +1,5 @@
 -- kmawesome: My configuration of awesome window manager
--- Copyright (c) 2012-2013 Kazuki Maeda <kmaeda@users.sourceforge.jp>
+-- Copyright (c) 2012--2015 Kazuki Maeda <kmaeda@kmaeda.net>
 
 local awful = require('awful')
 awful.rules = require('awful.rules')
