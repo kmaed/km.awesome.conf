4d2b409fe2150cb1bd9d88ff6f525da608ec77ac
Kazuki Maeda <kmaeda@kmaeda.net>
2018-06-27 16:26:43 +0900

    s-o: screen saver

--- rc.lua
+++ rc.lua
@@ -255,6 +255,7 @@ local globalkeys = awful.util.table.join(
    awful.key({}, 'XF86Display', function () awful.spawn('/home/kmaeda/vga.sh') end),
    awful.key({}, 'XF86ScreenSaver', function () awful.spawn('xscreensaver-command -lock') end),
    awful.key({}, 'XF86Sleep', function () awful.spawn(sleepcommand) end),
+   awful.key({modkey}, 'o', function () awful.spawn('xscreensaver-command -lock') end),
    awful.key({}, 'XF86MonBrightnessUp', function () awful.spawn('xbacklight +10') end),
    awful.key({}, 'XF86MonBrightnessDown', function () awful.spawn('xbacklight -10') end),
 
